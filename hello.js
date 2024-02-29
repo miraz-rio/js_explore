0508504b9e0d87fb22b268d8cd3f62f5167863ee
miraz-rio <mirazbd99@gmail.com>
2024-02-29 20:43:00 +0600

    Operator

--- hello.js
+++ hello.js
@@ -14,3 +14,33 @@ console.log(promise.toUpperCase());
 console.log(promise.indexOf("is"));
 // for split
 console.log(promise.split(" "));
+
+
+var num1 = "25";
+var num2 = 40.5;
+num1 = parseFloat(num1);
+num2 = parseInt(num2);
+console.log(num1 + num2);
+
+var total = num1 + num2;
+console.log(total);
+
+var total = num1 - num2;
+console.log(total);
+
+var total = num1 * num2;
+console.log(total);
+
+var total = num1 / num2;
+console.log(total);
+
+var total = num1 % num2;
+console.log(total);
+
+num1++;
+console.log(num1);
+
+num1--;
+console.log(num1);
+
+
