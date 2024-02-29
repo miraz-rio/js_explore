24351a36791e554be12459aa4af51b81ddbf1d60
miraz-rio <mirazbd99@gmail.com>
2024-02-29 17:19:27 +0600

    split

--- hello.js
+++ hello.js
@@ -1,10 +1,16 @@
-/* for string use ""
-
-find the type of variable, write typeof
-console.log(typeof example); */
-
 var bananaPrice = 5;
 console.log(bananaPrice);
+// for string use ""
 bananaPrice = "Janina"
 console.log(bananaPrice);
-console.log(typeof bananaPrice); 
+// find the type of variable, write typeof
+console.log(typeof bananaPrice);
+
+var promise = "A Promise is a proxy for a value";
+console.log(promise.toLowerCase());
+// for upperCase
+console.log(promise.toUpperCase());
+// for find position 
+console.log(promise.indexOf("is"));
+// for split
+console.log(promise.split(" "));
