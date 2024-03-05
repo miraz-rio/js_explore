3fd83a44e6a8240f7e0db68db1b7564c1d42dae6
miraz-rio <mirazbd99@gmail.com>
2024-03-06 00:38:15 +0600

    conditional decision, if-elser

---
+++ conditionals.js
@@ -0,0 +1,16 @@
+/* var price = 10;
+if(price <10){
+    console.log("I will buy the product");
+}
+else{
+    console.log("I will chose another one")
+} */
+
+var light = true;
+var watt = 50;
+if(light == true && watt > 50){
+    console.log("I will buy the light");
+}
+else{
+    console.log("I don't buy it");
+}
