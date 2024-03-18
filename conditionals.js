2550f8cf02fcca3b2295e0c04a8782587b3c79cd
miraz-rio <mirazbd99@gmail.com>
2024-03-18 14:42:58 +0600

    conditional decision, if-else

--- conditionals.js
+++ conditionals.js
@@ -6,11 +6,24 @@ else{
     console.log("I will chose another one")
 } */
 
-var light = true;
+/* var light = true;
 var watt = 50;
 if(light == true && watt > 50){
     console.log("I will buy the light");
 }
 else{
     console.log("I don't buy it");
+} */
+
+var job = true;
+var Salary = 20000;
+
+if (job == true && Salary > 15000){
+    console.log("You are perfect")
+}
+else if(job == true){
+    console.log("Thinking about you")
+}
+else(){
+    console.log("You disqualify")
 }
